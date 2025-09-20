55b4c4db8cf91079fd84d8fb3c7a528ca766a809
Cinemadata <gouthamxbet@gmail.com>
2025-09-21 00:21:53 +0530

    Update fetchShows.js

--- fetchShows.js
+++ fetchShows.js
@@ -1,5 +1,6 @@
 // ================= Node.js Script: fetchShows.js =================
-import fetch from "node-fetch";
+import fs from "fs";
+import path from "path";
 
 
 // ================= Cities (All Telangana) ================= //
